7a17373ac8f17f41a5498029eeb216263b33f3c3
Andrea Coronese <sixpounder@protonmail.com>
2021-05-25 09:06:56 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,24 @@ fn main() {
                         .short("d")
                         .value_name("DATA")
                 )
+                .arg(
+                    clap::Arg::with_name("format")
+                        .required(false)
+                        .short("f")
+                        .help("Sets the output format")
+                )
+                .arg(
+                    clap::Arg::with_name("INPUT")
+                        .required(true)
+                        .index(1)
+                        .help("Sets the input file to use")
+                )
+                .arg(
+                    clap::Arg::with_name("OUTPUT")
+                        .required(false)
+                        .index(2)
+                        .help("Sets the output image. If not specified, output is emitted to stdout")
+                )
         )
         .subcommand(
             clap::SubCommand::with_name("decode")
@@ -25,17 +43,58 @@ fn main() {
         );
     let matches = app.clone().get_matches();
 
-    if let Some("encode") = matches.subcommand_name() {
-        let input_path;
-        run_encode();
-    } else if let Some(_) = matches.subcommand_matches("decode") {
-        run_decode()
-    } else {
-        app.print_long_help().unwrap();
+    match matches.subcommand_name() {
+        Some("encode") => {
+            if let Some(subcommand_args) = matches.subcommand_matches("encode") {
+                let input_reader = match subcommand_args.value_of("INPUT") {
+                    Some(arg) => {
+                        Box::new(std::fs::File::open(arg).unwrap()) as Box<dyn std::io::Read>
+                    }
+                    None => {
+                        Box::new(std::io::stdin())
+                    }
+                };
+                let out_writer = match subcommand_args.value_of("OUTPUT") {
+                    Some(arg) => {
+                        Box::new(std::fs::File::create(arg).unwrap()) as Box<dyn std::io::Write>
+                    }
+                    None => {
+                        Box::new(std::io::stdout())
+                    }
+                };
+
+                run_encode(input_reader.as_ref(), "", out_writer.as_ref());
+            }
+        },
+        Some("decode") => {
+            if let Some(subcommand_args) = matches.subcommand_matches("decode") {
+                let input_reader = match subcommand_args.value_of("INPUT") {
+                    Some(arg) => {
+                        Box::new(std::fs::File::open(arg).unwrap()) as Box<dyn std::io::Read>
+                    }
+                    None => {
+                        Box::new(std::io::stdin())
+                    }
+                };
+                let out_writer = match subcommand_args.value_of("OUTPUT") {
+                    Some(arg) => {
+                        Box::new(std::fs::File::create(arg).unwrap()) as Box<dyn std::io::Write>
+                    }
+                    None => {
+                        Box::new(std::io::stdout())
+                    }
+                };
+
+                run_decode();
+            }
+        }
+        _ => {
+            app.print_long_help().unwrap();
+        }
     }
 }
 
-fn run_encode(path: &str, data: &str, out: &str) {
+fn run_encode(input: &dyn std::io::Read, data: &str, out: &dyn std::io::Write) {
     let path = "";
     let encoder = seagul_core::encoder::ImageEncoder::from(path);
 
