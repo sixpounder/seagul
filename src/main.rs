edda94a289d369b372057dcf1a8941f9840ce70c
Andrea Coronese <sixpounder@protonmail.com>
2021-05-25 15:49:17 +0200

    basic cli working

--- src/main.rs
+++ src/main.rs
@@ -13,37 +13,71 @@ fn main() {
         .subcommand(
             clap::SubCommand::with_name("encode")
                 .about("Encodes data into an image")
-                .arg(clap::Arg::with_name("data").short("d").value_name("DATA"))
+                .arg(
+                    clap::Arg::with_name("data")
+                        .short("d")
+                        .long("data")
+                        .value_name("DATA")
+                        .help(
+                            "Sets the data to encode in the final output"
+                        ),
+                )
                 .arg(
                     clap::Arg::with_name("format")
                         .required(false)
                         .short("f")
-                        .help("Sets the output format"),
+                        .long("format")
+                        .value_name("FORMAT")
+                        .help("Sets the output format. Supported types are png, jpeg and bmp"),
                 )
-                .arg(clap::Arg::with_name("INPUT").required(false).index(1).help(
-                    "Sets the input file to use. Attempts to read from stdin if not specified.",
-                ))
+                .arg(
+                    clap::Arg::with_name("INPUT")
+                        .required(false)
+                        .index(1)
+                        .help(
+                            "The path to the input image file to use. \
+                            Attempts to read from stdin if not specified. \
+                            This argument is ignored if the \"data\" option is also specified.",
+                        )
+                )
                 .arg(
                     clap::Arg::with_name("OUTPUT")
                         .required(false)
                         .index(2)
                         .help(
-                            "Sets the output image. If not specified, output is emitted to stdout",
+                            "The output image path on the file system. \
+                            If not specified, output is emitted to stdout",
                         ),
                 ),
         )
         .subcommand(
             clap::SubCommand::with_name("decode")
                 .about("Attempt to decode a message from an image")
-                .arg(clap::Arg::with_name("INPUT").required(false).index(1).help(
-                    "Sets the input file to use. Attempts to read from stdin if not specified.",
-                ))
+                .arg(
+                    clap::Arg::with_name("decode_marker")
+                        .required(false)
+                        .short("m")
+                        .long("marker")
+                        .value_name("MARKER")
+                        .help(
+                            "Decode until this sequence of bytes is found (or the input file ends)",
+                        )
+                    )
+                .arg(
+                    clap::Arg::with_name("INPUT")
+                        .required(false)
+                        .index(1)
+                        .help(
+                            "Sets the input file to use. Attempts to read from stdin if not specified.",
+                        )
+                    )
                 .arg(
                     clap::Arg::with_name("OUTPUT")
                         .required(false)
                         .index(2)
                         .help(
-                            "Sets the output image. If not specified, output is emitted to stdout",
+                            "Sets the output file for the decoded content. \
+                            If not specified, output is emitted to stdout",
                         ),
                 ),
         );
@@ -55,7 +89,8 @@ fn main() {
         Some("encode") => {
             if let Some(subcommand_args) = matches.subcommand_matches("encode") {
                 let (mut input_reader, mut output_writer) = subcommand_channels(subcommand_args);
-                match run_encode(&mut input_reader, "", &mut output_writer) {
+                let data_to_encode = subcommand_args.value_of("data").unwrap_or("");
+                match run_encode(&mut input_reader, data_to_encode, &mut output_writer) {
                     Ok(_) => {}
                     Err(e) => {
                         cli_error(e);
@@ -66,7 +101,15 @@ fn main() {
         Some("decode") => {
             if let Some(subcommand_args) = matches.subcommand_matches("decode") {
                 let (mut input_reader, mut output_writer) = subcommand_channels(subcommand_args);
-                match run_decode(&mut input_reader, &mut output_writer) {
+
+                let marker = match subcommand_args.value_of("decode_marker") {
+                    Some(marker) => {
+                        Some(marker.as_bytes())
+                    },
+                    None => None
+                };
+
+                match run_decode(&mut input_reader, &mut output_writer, marker) {
                     Ok(_) => {}
                     Err(e) => {
                         cli_error(e);
@@ -82,7 +125,9 @@ fn main() {
 
 fn subcommand_channels(subcommand: &ArgMatches) -> (Box<dyn Read>, Box<dyn Write>) {
     let input_reader = match subcommand.value_of("INPUT") {
-        Some(arg) => Box::new(std::fs::File::open(arg).unwrap()) as Box<dyn std::io::Read>,
+        Some(arg) => {
+            Box::new(std::fs::File::open(arg).unwrap()) as Box<dyn std::io::Read>
+        },
         None => Box::new(std::io::stdin()),
     };
     let out_writer = match subcommand.value_of("OUTPUT") {
@@ -109,12 +154,14 @@ where
     }
 }
 
-fn run_decode<R, W>(input: &mut R, out: &mut W) -> Result<(), std::io::Error>
+fn run_decode<'a, R, W>(input: &mut R, out: &mut W, marker: Option<&'a [u8]>) -> Result<(), std::io::Error>
 where
     R: std::io::Read,
     W: std::io::Write,
 {
-    let decoder = seagul_core::decoder::ImageDecoder::from(input);
+    let mut decoder = seagul_core::decoder::ImageDecoder::from(input);
+
+    decoder.until_marker(marker);
 
     match decoder.decode() {
         Ok(image) => image.write(out),
